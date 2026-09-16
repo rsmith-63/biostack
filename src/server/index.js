da6c6c5643284091f8a12c873bb7e3051d976d79
rsmith-63 <rsmith_63@yahoo.com>
2026-09-16 17:47:31 -0400

    try new port binding

--- src/server/index.js
+++ src/server/index.js
@@ -382,8 +382,8 @@ const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
 if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  app.listen(PORT, HOST, () => {
+    console.log(`Server running on http://${HOST}:${PORT}`);
   });
 }
 
